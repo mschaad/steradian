4cd21a666a30bf64540c2766ba0e42f1d674ec92
Matthew Schaad <matthew.schaad@gmail.com>
2016-07-14 21:19:22 -0400

    moved getEquivalentBaseTerms fn to UnitExpression.js

--- src/Unit.js
+++ src/Unit.js
@@ -1,24 +1,26 @@
-define(['src/UnitType', 'src/Term', 'src/Dimensions'], function(UnitType, Term, Dimensions) {
-	function Unit(name, type, symbol, scale) {
-		this.name = name;
-		this.type = type;
-		this.symbol = symbol;
-		this.scale = scale;
-	}
-		
-	Unit.prototype = {
-		getDimensions: function() {
-			if (!this._dimensions) {
-				var dim = [];
-				dim[UnitType[this.type]] = 1;
-				this._dimensions = new Dimensions(dim);
-			}
-			return this._dimensions;
-		},
-		getTerms: function() {
-			var terms = [new Term(this, 1)];
-			return terms;
+define(['src/UnitType', 'src/Term', 'src/Dimensions'], 
+	function(UnitType, Term, Dimensions) {
+		function Unit(name, type, symbol, scale) {
+			this.name = name;
+			this.type = type;
+			this.symbol = symbol;
+			this.scale = scale;
 		}
-	};
-	return Unit;
-});
+			
+		Unit.prototype = {
+			getDimensions: function() {
+				if (!this._dimensions) {
+					var dim = [];
+					dim[UnitType[this.type]] = 1;
+					this._dimensions = new Dimensions(dim);
+				}
+				return this._dimensions;
+			},
+			getTerms: function() {
+				var terms = [new Term(this, 1)];
+				return terms;
+			}
+		};
+		return Unit;
+	}
+);
--- src/UnitExpression.js
+++ src/UnitExpression.js
@@ -8,13 +8,18 @@ define([], function() {
 			return this._terms();
 		},
 		mult: function(rhs) {
-			throw 'not implemented';
+			var terms = flatten([this.terms(), rhs.terms()])
+			terms = combineLikeTerms(terms);
+			return new UnitExpression(terms);
 		},
 		div: function(rhs) {
-			throw 'not implemented';
+			var inverseRhs = new UnitExpression(rhs.terms().map(invert));
+			return this.mult(inverseRhs);
 		},
 		pow: function(power) {
-			var exponentiatedTerms = this.terms().map(function(t) { return t.pow(power); });
+			var exponentiatedTerms = this.terms().map(function(t) { 
+				return new Term(t.unit(), t.power() * power); 
+			});
 			return new UnitExpression(exponentiatedTerms);
 		},
 		simplify: function() {
@@ -22,6 +27,65 @@ define([], function() {
 			return new UnitExpression(baseTerms);
 		},
 	};
+	
+	function unitProperty(term) {
+		return term.unit();
+	}
+	
+	function combineLikeTerms(terms) {
+		terms = groupBy(terms, unitProperty);
+		terms = terms.map(function(group) {
+			return new Term(group.key, sumPowersOf(group.items));
+		});
+		return terms;
+	}
+	
+	function invert(term) {
+		return new Term(term.unit(), -term.power());
+	}
+	
+	function flatten(arrays) {
+		var flattened = [];
+		flattened.length = arrays.reduce(function(acc, arr) { return acc + arr.length; }, 0);
+		arrays.forEach(function(a) { 
+			Array.prototype.push.apply(flattened, a);
+		});
+		return flattened;
+	}
+	
+	function groupBy(arr, keySelector) {
+		var groups = {};
+		for (var i = 0; i < arr.length; i++) {
+			var item = arr[i];
+			var key = keySelector(item);
+			var group = groups[key] || { key: key, items: [] };
+			group.items.push(item);
+			groups[key] = group;
+		}
+		return groups;
+	}
+	
+	function sumPowersOf(terms) {
+		var sum = terms.reduce(function(acc, t) { acc + t.power() }, 0);
+		return sum;
+	}
+		
+	function getEquivalentBaseTerms(term) {
+		var unit = term.unit();
+		if (u.isBase()) {
+			return u.terms();
+		}
+		else {
+			var terms;
+			terms = u.terms();
+			terms = flatten(terms.map(getEquivalentBaseTerms));
+			terms = terms.map(function(innerTerm) {
+				return new Term(innerTerm.unit(), innerTerm.power() * term.power());
+			});
+			terms = combineLikeTerms(terms);
+			return terms;
+		}
+	}
 	
 	return UnitExpression;
 });
