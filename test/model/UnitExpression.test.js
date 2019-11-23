29d59d958d7fd6f94df3bca238630c41f6506429
Matthew Schaad <matthew.schaad@gmail.com>
2019-11-23 18:14:27 -0500

    uncommented UnitExpression toString() tests

--- test/model/UnitExpression.test.js
+++ test/model/UnitExpression.test.js
@@ -167,65 +167,55 @@ function (
                 equal(-4, actualMap.second);
             });
         });
-        // var metersPerSecond = Sr.defineDerivedUnit([
-        //     { unit: "meter", power: 1 },
-        //     { unit: "second", power: -1 }
-        // ]);
 
-        // test("module returns object", function() {
-        //     ok(DerivedUnit);
-        // });
+        suite('toString', function() {
+            var Sr = StandardStrontiumFn();
 
-        // test("instances have appropriate methods", function() {
-        //     ok(metersPerSecond.getDimensions);
-        //     ok(metersPerSecond.getTerms);
-        // });
+            var meter = Sr.unit('meter');
+            var second = Sr.unit('second');
 
-        // suite('toString', function() {
-        //     var Sr = StandardStrontiumFn();
-
-        //     test('m/s', function() {
-        //         var meterPerSecond = Sr.defineDerivedUnit([
-        //             { unit: 'meter', power: 1 },
-        //             { unit: 'second', power: -1 }
-        //         ]);
+            test('m/s', function() {
+                var meterPerSecond = new UnitExpression([
+                    new Term(meter, 1),
+                    new Term(second, -1)
+                ]);
     
-        //         equal('m/s', meterPerSecond.toString());
-        //     });
+                equal('m/s', meterPerSecond.toString());
+            });
             
-        //     test("m/s^2", function() {
-        //         var meterPerSecondSq = Sr.defineDerivedUnit([
-        //             { unit: 'meter', power: 1 },
-        //             { unit: 'second', power: -2 }
-        //         ]);
+            test("m/s^2", function() {
+                var meterPerSecondSq = new UnitExpression([
+                    new Term(meter, 1),
+                    new Term(second, -2)
+                ]);
     
-        //         equal('m/s^2', meterPerSecondSq.toString());
-        //     });
+                equal('m/s^2', meterPerSecondSq.toString());
+            });
 
-        //     test("m s", function() {
-        //         var meterSecond = Sr.defineDerivedUnit([
-        //             { unit: 'meter', power: 1 },
-        //             { unit: 'second', power: 1 }
-        //         ]);
+            test("m s", function() {
+                var meterSecond = new UnitExpression([
+                    new Term(meter, 1),
+                    new Term(second, 1)
+                ]);
     
-        //         equal('m s', meterSecond.toString());
-        //     });
+                equal('m s', meterSecond.toString());
+            });
 
-        //     test("m^2", function() {
-        //         var meterSquared = Sr.defineDerivedUnit([
-        //             { unit: 'meter', power: 2 }
-        //         ]);
+            test("m^2", function() {
+                var meterSquared = new UnitExpression([
+                    new Term(meter, 2)
+                ]);
     
-        //         equal('m^2', meterSquared.toString());
-        //     });
+                equal('m^2', meterSquared.toString());
+            });
 
-        //     test("m^0", function() {
-        //         var meterSquared = Sr.defineDerivedUnit([
-        //             { unit: 'meter', power: 0 }
-        //         ]);
+            test("m^0", function() {
+                var meterSquared = new UnitExpression([
+                    new Term(meter, 0)
+                ]);
     
-        //         equal('', meterSquared.toString());
-        //     });
-        // });
+                equal('', meterSquared.toString());
+            });
+        });
     })
 });
