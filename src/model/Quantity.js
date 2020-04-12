fc8ef6766a175ff3d4aaba57ee6dc7f3daccc9f0
Matthew Schaad <matthew.schaad@gmail.com>
2020-04-12 17:37:35 -0400

    added arithmetic methods to Quantity

--- src/model/Quantity.js
+++ src/model/Quantity.js
@@ -9,6 +9,35 @@ define([], function() {
 		Object.freeze(this);
 	}
 
+	var ArithmeticOps = {
+		add: function add(lhs, rhs) {
+			var lhsUnits = lhs.units();
+			var convertedRhs = rhs.convertTo(lhsUnits);
+			var resultValue = lhs.value() + convertedRhs.value();
+			var resultQuantity = quantity(resultValue, lhsUnits, lhs.Sr); 
+			return resultQuantity
+		},
+		subtract: function add(lhs, rhs) {
+			var lhsUnits = lhs.units();
+			var convertedRhs = rhs.convertTo(lhsUnits);
+			var resultValue = lhs.value() - convertedRhs.value();
+			var resultQuantity = quantity(resultValue, lhsUnits, lhs.Sr); 
+			return resultQuantity
+		},
+		multiply: function(lhs, rhs) { 
+			var resultUnits = lhs.units().mult(rhs.units());
+			var resultValue = lhs.value() * rhs.value();
+			var resultQuantity = quantity(resultValue, resultUnits, lhs.Sr);
+			return resultQuantity;
+		},
+		divide: function(lhs, rhs) { 
+			var resultUnits = lhs.units().div(rhs.units());
+			var resultValue = lhs.value() / rhs.value();
+			var resultQuantity = quantity(resultValue, resultUnits, lhs.Sr);
+			return resultQuantity;
+		}
+	};
+
 	Quantity.prototype = {
 		convertTo: function(newUnits) {
 			return this.Sr.convert(this, newUnits);
@@ -19,10 +48,26 @@ define([], function() {
 		value: function() {
 			return this._value;
 		},
+		plus: function(rhs) {
+			return ArithmeticOps.add(this, rhs);
+		},
+		minus: function(rhs) {
+			return ArithmeticOps.subtract(this, rhs);
+		},
+		times: function(rhs) {
+			return ArithmeticOps.multiply(this, rhs);
+		},
+		dividedBy: function(rhs) {
+			return ArithmeticOps.divide(this, rhs);
+		},
 		toString: function toString(options) {
 			return this.value() + this.units().toString();
 		}
 	};
-	
+
+	function quantity(value, unitExpression, Sr) {
+		return new Quantity(unitExpression, value, Sr);
+	}
+
 	return Quantity;
 });
--- test/model/Quantity.test.js
+++ test/model/Quantity.test.js
@@ -27,11 +27,94 @@ function (mocha, chai, Strontium, StandardStrontiumFn, UnitExpression, Term) {
             
             test('can construct quantity of derived unit', function() {
                 var q = Sr.quantity("Newton", 6.123);
-                var quantityTerms = q.units().terms()
+                var quantityTerms = q.units().terms();
                 assert.equal(1, quantityTerms.length);
                 assert.equal("Newton", quantityTerms[0].unit().name);
             });
         })
+
+        suite("arithmetic", function() {
+            var Sr = StandardStrontiumFn();
+            var quantity = Sr.quantity;
+
+            var meter = Sr.unit('meter');
+            var foot = Sr.unit('foot')
+            var second = Sr.unit('second');
+
+            suite("addition", function() {
+                test("addition of equal types", function() {
+                    var q = quantity(meter, 3).plus(quantity(meter, 4));
+                    equal(q.toString(), "7m");
+                });
+                test("addition of compatible types", function() {
+                    var q = quantity(meter, 3);
+                    var q2 = q.plus(quantity(foot, 4));
+                    
+                    equal(q2.units().toString(), "m");
+                    closeTo(q2.value(), 4.2192, 5e-5);
+                });
+                test("addition of incompatible types", function() {
+                    throws(function() {
+                        var q = quantity(meter, 3).plus(quantity(second, 4));
+                    })
+                });
+            });
+
+            suite("subtraction", function() {
+                test("subtraction of equal types", function() {
+                    var q = quantity(meter, 3).minus(quantity(meter, 4));
+                    equal(q.toString(), "-1m");
+                });
+                test("subtraction of compatible types", function() {
+                    var q = quantity(meter, 3);
+                    var q2 = q.minus(quantity(foot, 4));
+                    
+                    equal(q2.units().toString(), "m");
+                    closeTo(q2.value(), 1.7808, 5e-5);
+                });
+                test("subtraction of incompatible types", function() {
+                    throws(function() {
+                        var q = quantity(meter, 3).minus(quantity(second, 4));
+                    })
+                });
+            });
+
+            suite("multiplication", function() {
+                test("multiplication of equal types", function() {
+                    var q = quantity(meter, 3).times(quantity(meter, 4));
+                    equal(q.toString(), "12m^2");
+                });
+                test("multiplication of like types", function() {
+                    var q = quantity(meter, 3);
+                    var q2 = q.times(quantity(foot, 4));
+                    
+                    equal(q2.units().toString(), "m ft");
+                    equal(q2.value(), 12);
+                });
+                test("multiplication of disparate types", function() {
+                    var q = quantity(meter, 3).times(quantity(second, 4));
+                    equal(q.toString(), "12m s");
+                });
+            });
+
+            suite("division", function() {
+                test("division of equal types", function() {
+                    var q = quantity(meter, 12).dividedBy(quantity(meter, 4));
+                    equal(q.toString(), "3");
+                });
+                test("division of like types", function() {
+                    var q = quantity(meter, 12);
+                    var q2 = q.dividedBy(quantity(foot, 4));
+                    
+                    equal(q2.units().toString(), "m/ft");
+                    equal(q2.value(), 3);
+                });
+                test("division of disparate types", function() {
+                    var q = quantity(meter, 12).dividedBy(quantity(second, 4));
+                    equal(q.toString(), "3m/s");
+                });
+            });
+        });
 
 		suite("toString", function() {
             var Sr = StandardStrontiumFn();
